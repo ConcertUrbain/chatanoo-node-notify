53a6a3ccb4eddbe238217393e8f6bf66371018de
Mathieu Desvé <mathieu.desve@me.com>
2012-08-10 22:18:08 +0000

    Add to Chatanoo

--- rsync.sh
+++ rsync.sh
@@ -1,2 +1,2 @@
 #!/bin/bash
-rsync -avz --delete -e ssh . "root@ns368978.ovh.net:/var/www/vhosts/dring93.org/node/notify" --exclude-from 'rsync.exclude'
+rsync -avz --delete -e ssh . "root@ns368978.ovh.net:/var/www/vhosts/chatanoo.org/core/notify/prod" --exclude-from 'rsync.exclude'
